7d2e5c3e40a1fc25f88e1c8ddfb07fa75f58017c
sakthisp432 <sakthisp432@gmail.com>
2022-11-25 22:13:22 +0530

    doned

---
+++ src/org/git/Sample.java
@@ -0,0 +1,14 @@
+package org.git;
+
+public class Sample {
+	private void g1() {
+		System.out.println("g1");
+	}
+	private void g2() {
+		System.out.println("g2");
+
+	}
+    public static void main(String[] args) {
+		
+	}
+}
